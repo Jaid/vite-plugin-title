951edf71fdc97087b63a718d050e09eb615b633d
Jaid <6216144+Jaid@users.noreply.github.com>
2026-05-16 14:00:06 +0100

    Initial implementation
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,38 @@
-const vitePluginTitle = () => {
-  return 'vite-plugin-title' // TODO Implement actual functionality
-}
+import type {PackageJson} from 'type-fest'
+import type {Plugin, ResolvedConfig} from 'vite'
 
-export default vitePluginTitle
+import fs from 'fs/promises' // eslint-disable-line
+import path from 'path'
+
+export default function titlePlugin(title?: string) {
+  let resolvedTitle = title
+  const plugin: Plugin = {
+    name: 'title',
+    transformIndexHtml() {
+      if (!resolvedTitle) {
+        return
+      }
+      return [
+        {
+          tag: 'title',
+          children: resolvedTitle,
+          injectTo: 'head-prepend',
+        },
+      ]
+    },
+  }
+  if (!resolvedTitle) {
+    plugin.configResolved = async (resolvedConfig: ResolvedConfig) => {
+      const packageJsonFile = path.join(resolvedConfig.root, 'package.json')
+      const packageJsonFileExists = await fs.exists(packageJsonFile)
+      if (packageJsonFileExists) {
+        const packageJsonString = await fs.readFile(packageJsonFile, 'utf8')
+        const packageJson = JSON.parse(packageJsonString) as PackageJson
+        if (packageJson.name) {
+          resolvedTitle = (packageJson.displayName as string | undefined) || packageJson.name
+        }
+      }
+    }
+  }
+  return plugin
+}
